fdc26361c9cf4c7f5d31368b1f5f97159c0f2b0e
cristian m <camunoz2@gmail.com>
2023-05-10 11:01:09 -0400

    vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import vercelServerless from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://nibaldoteinforma.cl",
   integrations: [tailwind()],
+  adapter: vercelServerless(),
 });
