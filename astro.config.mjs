32e3bfe06eb8f831f238ac2e1ae93fb4ba4ef071
cristian m <camunoz2@gmail.com>
2023-05-27 15:34:50 -0400

    added analytics with partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 import vercelStatic from "@astrojs/vercel/static";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://nibaldoteinforma.cl",
-  integrations: [tailwind()],
+  integrations: [tailwind(), partytown()],
   adapter: vercelStatic(),
-  output: "static",
+  output: "static"
 });
